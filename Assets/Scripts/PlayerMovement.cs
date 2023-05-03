30d8d34c8db374f32676575bfd572c7eefa29e73
KamilMusialowski <k.musialowski.km@gmail.com>
2023-05-03 13:44:13 +0200

    Added Witcher asset and MOVEMENT. 1st level 1st room done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,57 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speedMovement;
     [SerializeField] private float speedJump;
     private Rigidbody2D body;
+    private Animator anim;
+    private Vector3 transformLeft = new Vector3(-4, 4, 1);
+    private Vector3 transformRight = new Vector3(4, 4, 1);
+    private bool grounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
     }
 
     private void Update()
     {
-        body.velocity = new Vector3(Input.GetAxis("Horizontal")*speedMovement, body.velocity.y);
-        if(Input.GetKeyDown(KeyCode.Space))
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector3(horizontalInput*speedMovement, body.velocity.y);
+
+        //Flipping right and left
+        if(horizontalInput > 0.01f)
         {
-            body.velocity = new Vector2(body.velocity.x, speedJump);
+            transform.localScale = transformRight;
+        } else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = transformLeft;
         }
 
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        //Set animator params
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speedJump);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
